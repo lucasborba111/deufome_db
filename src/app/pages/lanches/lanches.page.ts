08b6ee1f898703a562370dff3ca2a44a015c927f
DedeApenas <andrelromao@hotmail.com>
2021-11-02 22:04:20 -0300

    Update postar

--- src/app/pages/lanches/lanches.page.ts
+++ src/app/pages/lanches/lanches.page.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NavController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+import firebase from 'firebase/app';
 @Component({
   selector: 'app-lanches',
   templateUrl: './lanches.page.html',
   styleUrls: ['./lanches.page.scss'],
 })
 export class LanchesPage implements OnInit {
+  posts = []
+  constructor(
+    private navCtrl: NavController,
+    private storage: Storage
+  ) { }
+ 
+  ionViewWillEnter(){
+    this.fetchPosts();
+  }
+  
+  fetchPosts(){
+    this.posts = [];
+    firebase.database().ref('posts').on('value', snapshot=>{
+      let result = snapshot.val();
+      console.log('result = ', result);
 
-  constructor() { }
+      for(let i in result){
+        var element = result[i];
+        let post = {
+          photo: element.photo,
+          title: element.title,
+          description: element.description,
+        }
+        this.posts.push(post);
+      }
+      console.log('this.posts = ', this.posts);
+    })
+    
+    //this.posts = [];
+    //this.storage.get('postsArray').then(res =>{
+      //console.log('postsArray', res);
+      //this.posts = res;
+    //})
+  }
+  
+  postDetails(post){
+    this.navCtrl.navigateForward(['postdetails', {data: JSON.stringify(post)}])
+  }
 
-  ngOnInit() {
+  addArticle(){
+    this.navCtrl.navigateForward('add');
+  }
+  
+  async ngOnInit() {
+    await this.storage.create();
   }
 
   hidden = false;
